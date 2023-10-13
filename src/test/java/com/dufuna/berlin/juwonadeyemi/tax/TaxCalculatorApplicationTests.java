6457c670461c7de87eca3a693333b7f8e3aa8014
samju200 <samju7778@gmail.com>
2023-10-13 22:59:45 +0100

    Starting with spring started

---
+++ src/test/java/com/dufuna/berlin/juwonadeyemi/tax/TaxCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dufuna.berlin.juwonadeyemi.tax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

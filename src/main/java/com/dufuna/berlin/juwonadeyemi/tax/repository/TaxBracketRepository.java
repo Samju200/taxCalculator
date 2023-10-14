b9f1e5c637b207bceeb987e7a2f1d3685ecdad26
samju200 <samju7778@gmail.com>
2023-10-14 16:15:48 +0100

    Tax bracket repository using Jpa data management

---
+++ src/main/java/com/dufuna/berlin/juwonadeyemi/tax/repository/TaxBracketRepository.java
@@ -0,0 +1,8 @@
+package com.dufuna.berlin.juwonadeyemi.tax.repository;
+
+import com.dufuna.berlin.juwonadeyemi.tax.entity.TaxBracket;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaxBracketRepository extends JpaRepository<TaxBracket, Long> {
+
+}

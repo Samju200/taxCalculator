f5d1b64011eb5a27a135664ca38d3c0b5f3bd462
samju200 <samju7778@gmail.com>
2023-10-17 19:56:05 +0100

    files

---
+++ src/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.dufuna.berlin.juwonadeyemi.tax"},{"l":"com.dufuna.berlin.juwonadeyemi.tax.entity"},{"l":"com.dufuna.berlin.juwonadeyemi.tax.repository"},{"l":"com.dufuna.berlin.juwonadeyemi.tax.service"}];updateSearchResults();
